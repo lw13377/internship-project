b94876c487b40453cbef46b2f1831fb7a103a8da
lw13377 <eabrahamyan1717@gmail.com>
2024-09-03 13:38:21 -0700

    Task 4 Browserstack

--- features/environment.py
+++ features/environment.py
@@ -7,9 +7,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
 
-def browser_init(context):
+# def browser_init(context):
+def browser_init(context, scenario_name):
+
     """
-    :param context: Behave context
+    # :param context: Behave context
     """
 
     # options = webdriver.ChromeOptions()
@@ -21,9 +23,22 @@ def browser_init(context):
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
 
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
+    bs_user = 'erikabrahamyan_MtlTYk'
+    bs_key = 'YCcBhXftTypwTx7Eqxhg'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        "browserName": "chrome",
+        "sessionName": scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -34,11 +49,17 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
+
+
 def before_step(context, step):
     print('\nStarted step: ', step)
+
+
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
+
+
 def after_scenario(context, feature):
     context.driver.quit()
