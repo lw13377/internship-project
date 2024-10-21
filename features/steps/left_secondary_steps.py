bb7a3bbf250ea043aa51285718bcf2fb9b091384
lw13377 <eabrahamyan1717@gmail.com>
2024-10-20 19:03:31 -0700

    Task 13

--- features/steps/left_secondary_steps.py
+++ features/steps/left_secondary_steps.py
@@ -2,24 +2,26 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
+
 @then("Open Secondary Page from the left")
 def secondary_steps(context):
     context.app.secondary_page.click_on_secondary_left()
+
 
 @when("Verify that Secondary Page Opened")
 def verify_secondary_opened(context):
     context.app.secondary_page.verify_secondary_url()
     sleep(2)
 
+
 @then("Click on Next Page to the end")
 def next_page(context):
-    for i in range (98):
-        context.app.secondary_page.next_page()
+    context.app.secondary_page.next_page()
+
 
 @then("Click on Previous page to the end")
 def previous_page(context):
-    for i in range (98):
-        context.app.secondary_page.prev_page()
+    context.app.secondary_page.prev_page()
 
 
 
--- pages/secondary_page.py
+++ pages/secondary_page.py
@@ -7,6 +7,8 @@ class SecondaryPage(Page):
     SECONDARY_PAGE = By.XPATH , "//div[@class='div-block-28']//div[text()='Secondary']"
     NEXT_PAGE = By.CSS_SELECTOR , "[class='pagination__button w-inline-block']"
     PREVIOUS_PAGE = By.CSS_SELECTOR , "[class='pagination__button']"
+    TOTAL_PAGE = By.CSS_SELECTOR , "[wized='totalPageProperties']"
+    CURRENT_PAGE = By.CSS_SELECTOR , "[wized='currentPageProperties']"
 
     def click_on_secondary_left(self):
         self.click(*self.SECONDARY_PAGE)
@@ -14,12 +16,22 @@ class SecondaryPage(Page):
     def verify_secondary_url(self):
         actual_url = self.driver.current_url
         expected_url = "https://soft.reelly.io/secondary-listings"
-        assert self.driver.current_url == expected_url, f"expected {expected_url}, but got {self.driver.current_url}"
+        assert actual_url == expected_url, f"expected {expected_url}, but got {actual_url}"
 
     def next_page(self):
-        self.click(*self.NEXT_PAGE)
+        total_page = int(self.find_element(*self.TOTAL_PAGE).text)
+        while True:
+            current_page = int(self.find_element(*self.CURRENT_PAGE).text)
+            if current_page >= total_page:
+                break
+            self.wait_and_click(*self.NEXT_PAGE)
 
     def prev_page(self):
-        self.click(*self.PREVIOUS_PAGE)
+        while True:
+            current_page = int(self.find_element(*self.CURRENT_PAGE).text)
+            if current_page <= 1:
+                break
+            self.wait_and_click(*self.PREVIOUS_PAGE)
+
 
 
